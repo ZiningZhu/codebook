20078c8c89833cc37baf6e02e0350bfa511eed62
ZiningZhu <zhuzining@126.com>
2017-05-20 23:57:23 +0800

    Added Grahm's algo

--- monotone.cpp
+++ monotone.cpp
@@ -23,3 +23,34 @@ void convex_hull(vector<pair<int, int> > pts, vector<pair<int, int> >& hull) {
 	}
 }
 
+// O(N log N) Grahm's scan algorithm for 2d convex hull.
+// Python implementation on Leetcode Q587
+// First select a pivot point s0 to be the one with smallest y (smallest x as tiebreaker)
+// Then sort all points in increasing order of phi
+class ConvexHullSolution:
+	def CH(self, points):
+        # Computes the cross product of vectors p1p2 and p2p3
+        # value of 0 means points are colinear; < 0, cw; > 0, ccw
+        def cross(p1, p2, p3):
+            return (p2.x - p1.x)*(p3.y - p1.y) - (p2.y - p1.y)*(p3.x - p1.x)
+
+        # Computes slope of line between p1 and p2
+        def slope(p1, p2):
+            return 1.0*(p1.y-p2.y)/(p1.x-p2.x) if p1.x != p2.x else float('inf')
+
+        # Find the smallest left point and remove it from points
+        start = min(points, key=lambda p: (p.x, p.y))
+        points.pop(points.index(start))
+
+        # Sort points so that traversal is from start in a ccw circle.
+        points.sort(key=lambda p: (slope(p, start), -p.y, p.x))
+
+        # Add each point to the convex hull.
+        # If the last 3 points make a cw turn, the second to last point is not in CH.
+        ans = [start]
+        for p in points:
+            ans.append(p)
+            while len(ans) > 2 and cross(ans[-3], ans[-2], ans[-1]) < 0:
+                ans.pop(-2)
+
+        return ans
